f0dc9d65866e8ba6ac68520946ff0332bd474565
Isas321 <javier.isas321@gmail.com>
2023-05-28 21:58:29 -0300

    Ejercicio 1

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,4 +1,7 @@
 ﻿// // See https://aka.ms/new-console-template for more information
+
+//******* Introduccion  ********
+
 // Console.WriteLine("Hello, World!");
 // int a;
 // int b;
@@ -7,21 +10,31 @@
 // Console.WriteLine("Valor de a: "+a);
 // Console.WriteLine("Valor de b: "+b);
 
+
+//************ Ejercicio 1 *************
 int num, cifra;
 int invertido=0;
-Console.WriteLine("Ingrese un numero entero positivo: ");
+Console.WriteLine("Ingrese un numero entero: ");
 bool control=int.TryParse(Console.ReadLine(), out num);
 if(control){
-    while(num>=10){
-        cifra = num%10;
-        num = num/10;
-        invertido = invertido*10 + cifra;  
-    }
-        invertido = invertido*10 + num;
+    if(num>0){
+        while(num>=10){
+            cifra = num%10;
+            num = num/10;
+            invertido = invertido*10 + cifra;  
+            }
+            invertido = invertido*10 + num;
 
-    Console.WriteLine("Invertido: "+invertido);
+        Console.WriteLine("El numero ingresado invertido es: "+invertido);
+    } else{
+        Console.WriteLine("El numero ingresado es: " +num);
+    }
 } else{
     Console.WriteLine("No ingreso un numero entero");
 }
+
+
+
+
 
 
